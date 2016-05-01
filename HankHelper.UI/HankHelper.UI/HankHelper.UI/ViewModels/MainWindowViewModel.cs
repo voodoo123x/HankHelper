1030a290526fc063cf4b392472d00c20dd5859f7
voodoo123x <timoby86@gmail.com>
2016-05-01 01:05:40 -0400

    Continued UI development with more elements on screen.
    Continued UI development with more elements on screen.

--- HankHelper.UI/HankHelper.UI/HankHelper.UI/ViewModels/MainWindowViewModel.cs
+++ HankHelper.UI/HankHelper.UI/HankHelper.UI/ViewModels/MainWindowViewModel.cs
@@ -10,46 +10,23 @@ namespace HankHelper.UI
     public class MainWindowViewModel : INotifyPropertyChanged
     {
         #region Private Data Members
-        private string m_HardwareModel;
-        private string m_WorkingDirectory;
         private string m_ScriptTitle;
+        private string m_ScriptTextColor;
         private string m_ScriptBgColor;
+        private string m_ScriptWorkingDirectory;
         private int m_ScriptMajorVersion = 1;
         private int m_ScriptMinorVersion = 0;
-        private string m_ScriptTargetExec;
-        private string m_ScriptExecSwitches;
+        private string m_DriverName;
+        private string m_DriverDirectory;
+        private string m_DriverTargetExec;
+        private string m_DriverExecSwitches;
+        private string m_SaveName;
         private string m_SaveDirectory;
+        private IList<string> m_Colors;
+        private IList<DriverEntity> m_DriversToAdd = new List<DriverEntity>();
         #endregion
 
         #region Public Data Members
-        public string HardwareModel
-        {
-            get { return m_HardwareModel; }
-
-            set
-            {
-                if (value != m_HardwareModel)
-                {
-                    m_HardwareModel = value;
-                    NotifyPropertyChanged("HardwareModel");
-                }
-            }
-        }
-
-        public string WorkingDirectory
-        {
-            get { return m_WorkingDirectory; }
-
-            set
-            {
-                if (value != m_WorkingDirectory)
-                {
-                    m_WorkingDirectory = value;
-                    NotifyPropertyChanged("WorkingDirectory");
-                }
-            }
-        }
-
         public string ScriptTitle
         {
             get { return m_ScriptTitle; }
@@ -64,6 +41,20 @@ namespace HankHelper.UI
             }
         }
 
+        public string ScriptTextColor
+        {
+            get { return m_ScriptTextColor; }
+
+            set
+            {
+                if (value != m_ScriptTextColor)
+                {
+                    m_ScriptTextColor = value;
+                    NotifyPropertyChanged("ScriptTextColor");
+                }
+            }
+        }
+
         public string ScriptBgColor
         {
             get { return m_ScriptBgColor; }
@@ -74,6 +65,20 @@ namespace HankHelper.UI
                 {
                     m_ScriptBgColor = value;
                     NotifyPropertyChanged("ScriptBgColor");
+                }
+            }
+        }
+
+        public string ScriptWorkingDirectory
+        {
+            get { return m_ScriptWorkingDirectory; }
+
+            set
+            {
+                if (value != m_ScriptWorkingDirectory)
+                {
+                    m_ScriptWorkingDirectory = value;
+                    NotifyPropertyChanged("ScriptWorkingDirectory");
                 }
             }
         }
@@ -106,6 +111,76 @@ namespace HankHelper.UI
             }
         }
 
+        public string DriverName
+        {
+            get { return m_DriverName; }
+
+            set
+            {
+                if (value != m_DriverName)
+                {
+                    m_DriverName = value;
+                    NotifyPropertyChanged("DriverName");
+                }
+            }
+        }
+
+        public string DriverDirectory
+        {
+            get { return m_DriverDirectory; }
+
+            set
+            {
+                if (value != m_DriverDirectory)
+                {
+                    m_DriverDirectory = value;
+                    NotifyPropertyChanged("DriverDirectory");
+                }
+            }
+        }
+
+        public string DriverTargetExec
+        {
+            get { return m_DriverTargetExec; }
+
+            set
+            {
+                if (value != m_DriverTargetExec)
+                {
+                    m_DriverTargetExec = value;
+                    NotifyPropertyChanged("DriverTargetExec");
+                }
+            }
+        }
+
+        public string DriverExecSwitches
+        {
+            get { return m_DriverTargetExec; }
+
+            set
+            {
+                if (value != m_DriverExecSwitches)
+                {
+                    m_DriverExecSwitches = value;
+                    NotifyPropertyChanged("DriverExecSwitches");
+                }
+            }
+        }
+
+        public string SaveName
+        {
+            get { return m_SaveName; }
+
+            set
+            {
+                if (value != m_SaveName)
+                {
+                    m_SaveName = value;
+                    NotifyPropertyChanged("SaveName");
+                }
+            }
+        }
+
         public string SaveDirectory
         {
             get { return m_SaveDirectory; }
@@ -119,6 +194,47 @@ namespace HankHelper.UI
                 }
             }
         }
+
+        public IList<string> Colors
+        {
+            get
+            {
+                if (m_Colors == null)
+                {
+                    m_Colors = GetColors();
+                }
+     
+                return m_Colors;
+            }
+        }
+
+        public IList<DriverEntity> DriversToAdd
+        {
+            get { return m_DriversToAdd; }
+
+            set
+            {
+                if (value != m_DriversToAdd)
+                {
+                    m_DriversToAdd = value;
+                    NotifyPropertyChanged("DriversToAdd");
+                }
+            }
+        }
+        #endregion
+
+        #region Private Methods
+        private IList<string> GetColors()
+        {
+            IList<string> colors = new List<string>();
+
+            foreach (var c in Enum.GetValues(typeof(ScriptColor)))
+            {
+                colors.Add(c.ToString());
+            }
+
+            return colors;
+        }
         #endregion
 
         #region INotifyPropertyChanged
@@ -130,12 +246,38 @@ namespace HankHelper.UI
         }
         #endregion
 
-        #region Private Classes
-        private class CodeBlockEntity
+        #region Entity Classes
+        public class DriverEntity
         {
-            public string DriverName { get; set; }
-            public string InstallCommand { get; set; }
+            public string Name { get; set; }
+            public string Directory { get; set; }
+            public string Executable { get; set; }
+            public string Switches { get; set; }
         }
         #endregion
+
+        private enum ScriptColor
+        {
+            Black = '0',
+            Blue = '1',
+            Green = '2',
+            Aqua = '3',
+            Red = '4',
+            Purple = '5',
+            Yellow = '6',
+            White = '7',
+            Gray = '8',
+            LightBlue = '9',
+            LightGreen = 'A',
+            LightAqua = 'B',
+            LightRed = 'C',
+            LightPurple = 'D',
+            LightYellow = 'E',
+            BrightWhite = 'F'
+        }
+
+        //Code for getting character associated with each value
+        //ScriptColor sc = (ScriptColor)Enum.Parse(typeof(ScriptColor), value);
+        //System.Windows.MessageBox.Show(((char)sc).ToString());
     }
 }
